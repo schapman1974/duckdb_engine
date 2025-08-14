26d7e5caf4a02d7d26b0af9efef7636b3e22d2ff
schapman1974 <schapman1974@gmail.com>
2025-08-14 16:49:05 -0400

    Update config.py

--- duckdb_engine/config.py
+++ duckdb_engine/config.py
@@ -14,14 +14,13 @@ def get_core_config() -> Set[str]:
     # List of connection string parameters that are supported by MotherDuck
     # See: https://motherduck.com/docs/key-tasks/authenticating-and-connecting-to-motherduck/authenticating-to-motherduck/
     motherduck_config_keys = {"motherduck_token", "attach_mode", "saas_mode"}
-    additional_config_keys = {"init"}
 
     rows = (
         duckdb.connect(":memory:")
         .execute("SELECT name FROM duckdb_settings()")
         .fetchall()
     )
-    return {name for (name,) in rows} | motherduck_config_keys | additional_config_keys
+    return {name for (name,) in rows} | motherduck_config_keys
 
 
 def apply_config(
